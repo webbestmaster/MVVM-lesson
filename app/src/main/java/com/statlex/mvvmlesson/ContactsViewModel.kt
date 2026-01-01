b78700a726d5dd06f276c459c1978d24b1bc1521
dim <web.best.master@gmail.com>
2026-01-01 01:07:03 +0100

    WIP

--- app/src/main/java/com/statlex/mvvmlesson/ContactsViewModel.kt
+++ app/src/main/java/com/statlex/mvvmlesson/ContactsViewModel.kt
@@ -5,12 +5,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ContactsViewModel : ViewModel() {
     var bgColor by mutableStateOf(Color.Green)
         private set
 
     fun changeBgColor() {
-        bgColor = Color.Red
+        viewModelScope.launch {
+            delay(2000)
+            bgColor = Color.Red
+        }
     }
 }
